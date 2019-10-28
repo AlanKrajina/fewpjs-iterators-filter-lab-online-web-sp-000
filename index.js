a5d151c1186b8e11a2494148378116c09bf4e5fe
Alan Krajina <alankrajina88@gmail.com>
2019-10-28 17:04:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ function fuzzyMatch (drivers,string) {
 }
 
 function matchName (drivers,string) {
-  return drivers.filter(driver=> string.charAt(0) === driver.charAt(0))
+  return drivers.filter(driver=> string === driver.name)
 }
