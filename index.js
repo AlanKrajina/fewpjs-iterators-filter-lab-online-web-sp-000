481469896155aa6fc70895f5bc914246e7da4be4
Alan Krajina <alankrajina88@gmail.com>
2019-10-28 17:01:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,8 @@
 function findMatching (drivers,string) {
   return drivers.filter(driver=> string.toLowerCase() === driver.toLowerCase())
 }
+
+
+function fuzzyMatch (drivers,string) {
+  return drivers.filter(driver=> string.toLowerCase() === driver.charAt(0).toLowerCase())
+}
